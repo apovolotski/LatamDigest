c82d998d9e1865a1aa45f0741563e9d88edb5bf1
Alex Povolotski <apov@me.com>
2026-03-23 09:56:59 -0400

    Initial Commit

---
+++ LatamDigest/LatamDigestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LatamDigestApp.swift
+//  LatamDigest
+//
+//  Created by Alex on 2026-03-23.
+//
+
+import SwiftUI
+
+@main
+struct LatamDigestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
